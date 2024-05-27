286182d771efab6da413c0d93936145a3306f9ea
ThisIsJustAGuy <bbalint0404@gmail.com>
2024-05-27 12:51:39 +0200

    help

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,14 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', HomeController::class)->name('home');
 
 Route::resource('posts', PostsController::class);
+
+//php artisan make:model BookCategory -crR
+
+//composer require laravel/ui
+//php artisan ui bootstrap --auth
+//npm run build
+//chown -R www-data:www-data /var/www/storage
